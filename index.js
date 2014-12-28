90e2d5a06162ba63bbdfd88ba749bbeb5e1b0b31
Mathias Kretschek <mathias@kretschek.com.br>
2014-12-28 12:06:09 -0200

    Moved helpers to their own file

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports = module.exports = require('./lib/middleware');
 
 exports.Permission = require('./lib/permission');
 
-exports.any = require('./lib/permission').any;
-exports.all = require('./lib/permission').all;
-exports.has = require('./lib/permission').has;
+exports.any = require('./lib/helper').any;
+exports.all = require('./lib/helper').all;
+exports.has = require('./lib/helper').has;
